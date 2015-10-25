47f80d392e5be0e7a9bdb1e490e6f358fbe2101e
tizot <masset.camille@gmail.com>
2015-10-25 18:27:36 +0200

    Add models to admin website

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from vote.models import Student, Category, Video, Vote
+
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Category)
+admin.site.register(Video)
+admin.site.register(Vote)
