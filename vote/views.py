6583b128e8e663d7b1caab774be90ebedce6b7f6
tizot <masset.camille@gmail.com>
2015-11-07 18:56:54 +0100

    Get page from request

--- vote/views.py
+++ vote/views.py
@@ -22,14 +22,14 @@ class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    @decorators.list_route(methods=['get'])
+    @decorators.list_route(methods=['post'])
     def frankiz_auth_check(self, request):
         import logging
         logger = logging.getLogger(__name__)
 
         student = None
         response_data = {'valid': True, 'student': student}
-        RETURN_PAGE = request.get_host().encode()
+        RETURN_PAGE = request.data.get('page', 'http://jtx/vaneau/').encode()
         logger.error(RETURN_PAGE)
 
         if not "timestamp" in request.query_params.keys() or not "response" in request.query_params.keys() or not "hash" in request.query_params.keys():
@@ -59,12 +59,10 @@ class StudentViewSet(viewsets.ModelViewSet):
 
         return Response(response_data, 200)
 
-    @decorators.list_route(methods=['get'])
+    @decorators.list_route(methods=['post'])
     def frankiz_url(self, request):
-        RETURN_PAGE = request.get_host().encode()
-
         ts = str(int(time.time())).encode()
-        page = RETURN_PAGE
+        page = request.data.get('page', 'http://jtx/vaneau/').encode()
         r = json.dumps(["names","promo"]).encode()
         h = hashlib.md5(ts + page + FKZ_KEY + r).hexdigest()
         return Response("http://www.frankiz.net/remote?" + parse.unquote(parse.urlencode([('timestamp',ts),('site',page),('hash',h),('request',r)])), 200)
